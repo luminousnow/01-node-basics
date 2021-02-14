548c08ad9f32ed2213b9701dcae8d9f22f59867b
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-14 20:14:56 +0200

    refactored code on async await

--- contacts.js
+++ contacts.js
@@ -1,110 +1,78 @@
-const fs = require("fs");
+const { readFile, writeFile } = require("fs/promises");
 const path = require("path");
 const ID = require("nodejs-unique-numeric-id-generator");
 
 // DB url to contacts.json file
 const contactsPath = path.join("db", "contacts.json");
 
-function listContacts() {
-  fs.readFile(contactsPath, "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file", err);
-      return;
-    }
+// === GET all contacts ===
+async function listContacts() {
+  try {
+    const contctList = JSON.parse(await readFile(contactsPath, "utf8"));
 
-    try {
-      const contctList = JSON.parse(jsonString);
-      console.log("Seccess! Here is the All contact from contacts.json");
-      console.table(contctList);
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
+    console.log("Seccess! Here is the All contact from contacts.json");
+    console.table(contctList);
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file", err);
-      return;
-    }
-
-    try {
-      const contctList = JSON.parse(jsonString);
-
-      contctList.filter((contact) => {
-        if (contact.id === contactId) {
-          console.log("Seccess! You got it");
-          console.table([contact]);
-        }
-      });
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath, "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file", err);
-      return;
-    }
-
-    try {
-      const toRemove = JSON.parse(jsonString).filter(
-        (contact) => contact.id === contactId
-      );
-      const contctList = JSON.parse(jsonString).filter(
-        (contact) => contact.id !== contactId
-      );
-
-      // write a new collection of contact to contacts.json
-      fs.writeFile(contactsPath, JSON.stringify(contctList), (err) => {
-        if (err) {
-          console.log("Error reading file", err);
-          return;
-        }
-        console.log("Seccess! The contact was removed from contacts.json");
-        console.table(toRemove);
-      });
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
-}
-
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("Error reading file", err);
-      return;
-    }
-
-    try {
-      const id = Number(ID.generate(new Date().toJSON()));
-      const contact = {
-        id,
-        name,
-        email,
-        phone,
-      };
-      const contctList = [...JSON.parse(jsonString), contact];
-
-      // create new collection whis a new contact adn write to contacts.json
-      fs.writeFile(contactsPath, JSON.stringify(contctList), (err) => {
-        if (err) {
-          console.log("Error append data", err);
-          return;
-        }
-
-        console.log("Seccess! The new contact was add to contacts.json");
+// === GET contact by ID ===
+async function getContactById(contactId) {
+  try {
+    JSON.parse(await readFile(contactsPath, "utf8")).filter((contact) => {
+      if (contact.id === contactId) {
+        console.log("Seccess! You got it");
         console.table([contact]);
-      });
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
+      }
+    });
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+}
+
+// === REMOVE contact by ID ===
+async function removeContact(contactId) {
+  try {
+    const toRemove = JSON.parse(await readFile(contactsPath, "utf8")).filter(
+      (contact) => contact.id === contactId
+    );
+    const contctList = JSON.parse(await readFile(contactsPath, "utf8")).filter(
+      (contact) => contact.id !== contactId
+    );
+
+    // write a new collection of contact to contacts.json
+    await writeFile(contactsPath, JSON.stringify(contctList));
+
+    console.log("Seccess! The contact was removed from contacts.json");
+    console.table(toRemove);
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const id = await Number(ID.generate(new Date().toJSON()));
+    const contact = {
+      id,
+      name,
+      email,
+      phone,
+    };
+    const contctList = [
+      ...JSON.parse(await readFile(contactsPath, "utf8")),
+      contact,
+    ];
+
+    // create new collection whis a new contact adn write to contacts.json
+    await writeFile(contactsPath, JSON.stringify(contctList));
+
+    console.log("Seccess! The new contact was add to contacts.json");
+    console.table([contact]);
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
 }
 
 module.exports = {
